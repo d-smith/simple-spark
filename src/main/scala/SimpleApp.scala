0e530d692d4204f85c5ee6b329f8faba59678361
d-smith <doug.smith.mail@gmail.com>
2015-06-08 01:50:43 -0600

    Minor changes to run locally

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -4,12 +4,13 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
 	def main(args: Array[String]) {
-		val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-		val conf = new SparkConf().setAppName("Simple Application")
+		val logFile = "README.md" // Should be some file on your system
+		val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
 		val sc = new SparkContext(conf)
 		val logData = sc.textFile(logFile, 2).cache()
 		val numAs = logData.filter(line => line.contains("a")).count()
 		val numBs = logData.filter(line => line.contains("b")).count()
-		println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+		println("===========\nLines with a: %s, Lines with b: %s".format(numAs, numBs))
+		sc.stop()
 	}
 }
